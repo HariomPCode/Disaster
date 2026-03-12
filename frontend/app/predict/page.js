4aef4ce1c9fd36006c057143a94e77af92470497
Hariom pandey <pandeyhariom352@gmail.com>
2026-03-12 21:25:42 +0530

    making the frontend deployable

--- frontend/app/predict/page.js
+++ frontend/app/predict/page.js
@@ -395,7 +395,7 @@ export default function PredictPage() {
     setError(null);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://disaster-p4np.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
